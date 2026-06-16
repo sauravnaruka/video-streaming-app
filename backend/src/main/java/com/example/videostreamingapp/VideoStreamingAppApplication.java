6aa799a1b0accfc43ea91598d8c252903ca5513d
SauravNaruka <saurav.singh.n@gmail.com>
2026-06-16 06:54:17 +0530

    Fix package name

---
+++ backend/src/main/java/com/example/videostreamingapp/VideoStreamingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.videostreamingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamingAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/videostreamingapp/VideoStreamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videostreamingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
